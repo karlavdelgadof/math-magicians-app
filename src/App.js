e931618d2c6cff3c9730289e538a7446683fc491
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-03 00:23:53 -0400

    Modify main component with new app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import Navigation from './components/navBar';
+import Calculator from './components/Calculator';
+import Footer from './components/footer';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Navigation />
+        </header>
+        <main id="main-section">
+          <Calculator />
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
