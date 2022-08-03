11b07f40e96a9e00b941013427c183d2d3b0cd55
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-03 00:14:12 -0400

    Create app class based components

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer id="footer">
+        <p>Karla Delgado © 2022 All rights reserved.</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
