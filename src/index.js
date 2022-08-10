88aa32d07fa88f13be78c7627a8599442f0c7dd6
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-10 13:55:33 -0400

    Generate router and routes with respective paths

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Home from './pages/Home';
+import Calculator from './components/Calculator';
+import Quote from './pages/Quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
 );
