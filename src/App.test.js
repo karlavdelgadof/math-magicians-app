bb9254ebb835e009b61d852e6b50d040506f0390
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-11 12:00:05 -0400

    Add snaphot test for react components

---
+++ src/App.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+import Footer from "./components/Footer";
+import { BrowserRouter } from "react-router-dom";
+import Navigation from "./components/Navigation";
+
+describe("Component Testing", () => {
+  test("renders App component", () => {
+    const component = render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  test("renders Footer component", () => {
+    const component = render(<Footer />);
+    expect(component).toMatchSnapshot();
+  });
+
+  test("renders Navigation component", () => {
+    const component = render(
+      <BrowserRouter>
+        <Navigation />
+      </BrowserRouter>
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
