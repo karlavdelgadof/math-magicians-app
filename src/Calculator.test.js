7b4a102784bc029f78974d27bb3d91b7b398bbde
karlavdelgadof <karlavdelgadof@gmail.com>
2022-08-11 12:40:19 -0400

    Fix linter errors

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
-import Calculator from "./components/Calculator.js";
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Calculator from './components/Calculator';
 
-describe("User makes a calculation", () => {
-  test("Key 2, key 4 divide operation sign by key 3 ", () => {
-    const component = render(<Calculator />);
-    const calcDisplay = document.getElementById("calc-display");
+describe('User makes a calculation', () => {
+  test('Key 2, key 4 divide operation sign by key 3 ', () => {
+    render(<Calculator />);
+    const calcDisplay = document.getElementById('calc-display');
 
-    const keyTwo = screen.getByText("2");
-    const keyFour = screen.getByText("4");
-    const operator = screen.getByText("÷");
-    const keyThree = screen.getByText("3");
+    const keyTwo = screen.getByText('2');
+    const keyFour = screen.getByText('4');
+    const operator = screen.getByText('÷');
+    const keyThree = screen.getByText('3');
 
-    const equalOperator = screen.getByText("=");
+    const equalOperator = screen.getByText('=');
 
     fireEvent.click(keyTwo);
     fireEvent.click(keyFour);
@@ -20,6 +20,6 @@ describe("User makes a calculation", () => {
     fireEvent.click(keyThree);
     fireEvent.click(equalOperator);
 
-    expect(calcDisplay.textContent).toBe("8");
-  })
+    expect(calcDisplay.textContent).toBe('8');
+  });
 });
